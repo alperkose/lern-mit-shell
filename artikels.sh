3178f94c7ed184dd2b990b0f186b3a025f13d722
Alper Köse <alperkose@gmail.com>
2019-05-08 22:56:10 +0200

    Clear cluttered nouns and use only singular form

--- artikels.sh
+++ artikels.sh
@@ -5,7 +5,7 @@ function isArtikel  {
 	return $( [[ $1 == "der" ]] || [[ $1 == "die" ]] || [[ $1 == "das" ]] )
 }
 
-LINECOUNT=$(wc -l woerter.txt | grep -E '\d+' -o)
+LINECOUNT=$(wc -l nouns.txt | grep -E '\d+' -o)
 
 QUESTIONS=${1:-10}
 ANSWERS=0
@@ -14,7 +14,7 @@ CORRECT_ANSWERS=0
 while [ $ANSWERS -lt $QUESTIONS ]; do
 
 	RANDLINE=$((1 + RANDOM % $LINECOUNT))
-	WORD=$(head -n $RANDLINE woerter.txt | tail -n 1)
+	WORD=$(head -n $RANDLINE nouns.txt | tail -n 1)
 	ARTIKEL=$(echo $WORD | cut -c 1-3)
 	isArtikel $ARTIKEL || continue
 	
