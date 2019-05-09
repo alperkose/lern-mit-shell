372510da4f7c11effe69792f3c82173584a6a3b9
Alper Köse <alperkose@gmail.com>
2019-05-10 00:19:23 +0200

    Provide a way to support different files

--- artikels.sh
+++ artikels.sh
@@ -5,16 +5,29 @@ function isArtikel  {
 	return $( [[ $1 == "der" ]] || [[ $1 == "die" ]] || [[ $1 == "das" ]] )
 }
 
-LINECOUNT=$(wc -l nouns.txt | grep -E '\d+' -o)
-
+NOUNS_FROM="nouns.txt"
+while [[ $# > 1 ]]; do
+	case "$1" in
+		-f)
+			NOUNS_FROM="$2"
+			shift
+			;;
+		*) echo Unrecognized parameter: $1
+	esac
+	shift
+done
 QUESTIONS=${1:-10}
+
+
 ANSWERS=0
 CORRECT_ANSWERS=0
+
+LINECOUNT=$(wc -l $NOUNS_FROM | grep -E '\d+' -o)
 
 while [ $ANSWERS -lt $QUESTIONS ]; do
 
 	RANDLINE=$((1 + RANDOM % $LINECOUNT))
-	WORD=$(head -n $RANDLINE nouns.txt | tail -n 1)
+	WORD=$(head -n $RANDLINE $NOUNS_FROM | tail -n 1)
 	ARTIKEL=$(echo $WORD | cut -c 1-3)
 	isArtikel $ARTIKEL || continue
 	
